b5d8843b1b7e6c93cbca44e77b736ec778483698
eslamashraf98 <eslamashraf27@gmail.com>
2022-11-10 21:49:36 +0200

    new

---
+++ app.js
@@ -0,0 +1,64 @@
+// searchbar for #first mobile
+function myFunction(input, filter, ul, li = "sorry") {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            // sorry.style.display = "none";
+        } else {
+            li[i].style.display = "none";
+            // sorry.style.display = "block";
+        }
+    };
+};
+
+
+// searchbar for #second laptop
+function myFunctions() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInputs");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+
+        } else {
+            li[i].style.display = "none";
+        }
+    };
+    console.log(ul);
+};
+
+
+function vis() {
+    let search = document.getElementsByClassName("search")
+    let select = document.getElementById("head-title")
+    if (onclick = search) {
+        select.style.display = "none";
+    } else {
+        select.style.display = "block";
+    }
+};
+
+// uppage
+
+window.onscroll = function() {myFunctionUp()};
+
+let UpPage = document.querySelector(".uppage")
+
+function myFunctionUp() {
+  if (window.scrollY <= 1000) {
+    UpPage.style.opacity = 0;
+  } else {
+    UpPage.style.opacity = 0.5;
+    }
+};
+
