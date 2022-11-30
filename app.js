b1a62e5bcf72c5c3ed5342118cba7caa654a6cde
eslamashraf98 <eslamashraf27@gmail.com>
2022-11-30 22:48:41 +0200

    update

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+        document.onreadystatechange = function() {
+            if (document.readyState !== "complete") {
+                document.querySelector(
+                  "body").style.visibility = "hidden";
+                document.querySelector(
+                  "#loader").style.visibility = "visible";
+            } else {
+                document.querySelector(
+                  "#loader").style.display = "none";
+                document.querySelector(
+                  "body").style.visibility = "visible";
+            }
+        };
+
+
 // searchbar for #first mobile
 function myFunction() {
     var input, filter, ul, li, a, i;
@@ -64,3 +79,5 @@ function myFunctionUp() {
 var sold = document.querySelector(".sold")
 sold.innerHTML = "OUT OF STOCK";
 sold.style.background = "#f44";
+
+
