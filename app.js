dd75e308a0c6bf037cec10a9b9f8f9df3529ddc2
eslamashraf98 <eslamashraf27@gmail.com>
2023-02-11 22:19:50 +0200

    update

--- app.js
+++ app.js
@@ -126,3 +126,47 @@ Swal.fire({
 };
 
 
+
+
+
+
+// no screenshot
+function nocontext(e) {
+    var clickedTag = (e == null) ? event.srcElement.tagName :
+        e.target.tagName;
+    if (clickedTag == "IMG") {
+        alert(alertMsg);
+        return false;
+
+    }
+}
+var alertMsg = "sorry you can't make screenshot from this site";
+document.oncontextmenu = nocontext;
+
+
+//  document.addEventListener("keyup", function (e) {
+//     var keyCode = e.keyCode ? e.keyCode : e.which;
+//             if (keyCode == 44) {
+//                 stopPrntScr();
+//             }
+//         });
+// function stopPrntScr() {
+
+//             var inpFld = document.createElement("input");
+//             inpFld.setAttribute("value", ".");
+//             inpFld.setAttribute("width", "0");
+//             inpFld.style.height = "0px";
+//             inpFld.style.width = "0px";
+//             inpFld.style.border = "0px";
+//             document.body.appendChild(inpFld);
+//             inpFld.select();
+//             document.execCommand("copy");
+//             inpFld.remove(inpFld);
+//         }
+//        function AccessClipboardData() {
+//             try {
+//                 window.clipboardData.setData('text', "Access   Restricted");
+//             } catch (err) {
+//             }
+//         }
+//         setInterval("AccessClipboardData()", 300);
